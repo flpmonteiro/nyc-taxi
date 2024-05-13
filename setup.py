21ee829b84efb9853958af5369a391965da36de0
felipe <25232105+fpmonteiro@users.noreply.github.com>
2024-05-13 17:19:08 -0300

    Add requirements to setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,11 @@ setup(
     packages=find_packages(exclude=["nyc_taxi_tests"]),
     install_requires=[
         "dagster",
-        "dagster-cloud"
+        "dagster-cloud",
+        "dagster-duckdb",
+        "pandas",
+        "pyarrow",
+        "plotly",
     ],
     extras_require={"dev": ["dagster-webserver", "pytest"]},
 )
